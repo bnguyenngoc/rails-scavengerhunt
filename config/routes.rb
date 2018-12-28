b86436e55add04e7a1942f4d0c6407243fc75885
bach <bach.nguyen@merinio.com>
2018-12-27 22:29:54 -0500

    generates rails files and adds authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'pages#main'
+  get 'pages/main'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
